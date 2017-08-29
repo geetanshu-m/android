7f4fa18cca451590a23a20aa53176a9206eb2826
Geetanshu <mathur.geetanshu887@gmail.com>
2017-08-29 15:25:33 +0530

    second half3

--- app/src/main/java/com/example/pacman/first_app/First_activity.java
+++ app/src/main/java/com/example/pacman/first_app/First_activity.java
@@ -1,10 +1,16 @@
 package com.example.pacman.first_app;
 
+import android.content.Context;
 import android.content.res.AssetManager;
 import android.graphics.Typeface;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
 /**
@@ -19,7 +25,43 @@ public class First_activity extends AppCompatActivity {
         setContentView(R.layout.first_layout);
         AssetManager assetManager=getAssets();
         Typeface abc=Typeface.createFromAsset(assetManager,"font/Xanadu.ttf");
-        Button btn=(Button) findViewById(R.id.first_btn);
+        final Button btn=(Button) findViewById(R.id.first_btn);
         btn.setTypeface(abc);
+
+        //Vibration when  app open
+
+        /*
+        Vibrator vibrator=(Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        long a[]=new long[3];//declaration and instantiation
+        a[0]=1000;//initialization
+        a[1]=20;
+        a[2]=2000;
+        vibrator.vibrate(a,3);
+        */
+
+        //end
+
+        //vibration when button is clicked
+
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+
+                //For Vibrator
+                Vibrator vibrator=(Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                vibrator.vibrate(200);
+                //end
+                //For Playing Sound
+                    MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.button_1);
+                    mediaPlayer.start();
+                //end
+                //For Animation
+                Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.hi);
+                btn.startAnimation(animation);
+            }
+        });
+
+        //end
     }
 }
